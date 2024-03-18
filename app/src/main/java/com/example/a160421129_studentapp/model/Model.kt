a01a1be8a18685f58d225d0a436745f1e6025557
AhmadRamadhani129 <ahmad.ramadhani.aribowo@gmail.com>
2024-03-18 11:19:29 +0700

    Tugas Excersie 6 Week 4

---
+++ app/src/main/java/com/example/a160421129_studentapp/model/Model.kt
@@ -0,0 +1,10 @@
+package com.example.a160421129_studentapp.model
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
+
